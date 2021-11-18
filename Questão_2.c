a05b886d46a669d5633f9932354954bc6fadf49c
alexoliverumari <94552948+alexoliverumari@users.noreply.github.com>
2021-11-18 17:37:01 -0300

    Update Questão_2.c

--- Questão_2.c
+++ Questão_2.c
@@ -5,11 +5,11 @@ int main()
     float  base_maior, base_menor, altura, area;
     printf("Informe o valor em centimetros da base maior do trapézio: ");
     scanf("%f", &base_maior);
-    printf("Informe o valor em centimetros da base menor do trapézio: ");
+    printf("\nInforme o valor em centimetros da base menor do trapézio: ");
     scanf("%f", &base_menor);
-    printf("Informe o valor em centimetros da altura do trapézio: ");
+    printf("\nInforme o valor em centimetros da altura do trapézio: ");
     scanf("%f", &altura);
     area = ((base_maior + base_menor) * altura) / 2; 
-    printf("A area do trapézio é de %.1f centimetros", area);
+    printf("\nA area do trapézio é de %.1f centimetros", area);
     return 0;
 }
